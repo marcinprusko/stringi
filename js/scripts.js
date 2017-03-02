17fdeda37d19f2e85771bbc093517b03f2cac2ab
marcinprusko <marcinprusko@gmail.com>
2017-03-02 22:47:31 +0100

    strigni

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+﻿// plik scripts.js
+
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var upperanimal = animal.toUpperCase();
+
+console.log(upperanimal);
+var changes = text.replace('Papugi', upperanimal);
+
+console.log(changes);
+console.log(changes.length/2);
+
